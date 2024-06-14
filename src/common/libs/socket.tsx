9990478056b4522fca02bfba9aa37205ae325a95
Jack Anderson <whotfisjax@gmail.com>
2024-06-14 17:30:58 -0400

    v0

--- src/common/libs/socket.tsx
+++ src/common/libs/socket.tsx
@@ -1,3 +1,6 @@
 import { io } from 'socket.io-client';
+import type { Socket } from 'socket.io-client';
 
-export const socket = io(); // Remove type annotations
+import type { ClientToServer, ServerToClient } from '../types/socket.type';
+
+export const socket: Socket<ServerToClient, ClientToServer> = io();
